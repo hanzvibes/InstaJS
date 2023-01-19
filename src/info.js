d99e6d7ef863f34abcf680d7de5d681696a0c255
Raihan Ramadhani <64344985+hanzvibes@users.noreply.github.com>
2023-01-19 12:34:45 +0700

    Create info.js

---
+++ src/info.js
@@ -0,0 +1,20 @@
+const { chalk, inquirer, _, fs, instagram, print, delay } = require("./api.js");
+
+print(
+                    chalk`{bold.yellow
+  A simple NodeJS-based tool to help increase engagement on Instagram accounts and interact between accounts effortlessly. This tool was recoded from I Putu Jaya Adi Pranata.
+
+  
+  Find Bug ? Tell me on
+  —————————————————————————————————————————————————————
+  - Facebook   : https://fb.me/4RAEHAN
+  - Instagram  : https://instagram.com/hanzvibes
+  - GitHub     : https://github.com/hanzvibes
+  - Telegram   : https://t.me/InstaJS_Official
+  —————————————————————————————————————————————————————
+  
+  Special thanks to :
+  
+  - I Putu Jaya Adi Pranata
+  - All members of SGB Team
+  \n}`);
